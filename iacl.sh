081184caf2eea13f1e774fa2e36f1c4d15d2e23e
Ricardo Souza <30755281+riksouza@users.noreply.github.com>
2022-10-31 22:05:46 +0000

    Update iacl.sh

--- iacl.sh
+++ iacl.sh
@@ -1,69 +1,41 @@
 #!/bin/bash
 
-echo "criando diretorios" 
+echo "Criando diretórios..."
 
-mkdir /publico 
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
 
-mkdir /adm 
+echo "Criando grupos de usuários..."
 
-mkdir /ven 
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
 
-mkdir /sec 
+echo "Criando usuários..."
 
- 
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
-echo "criando grupos de usuarios" 
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-groupadd GRP_ADM 
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
-groupadd GRP_VEN 
+echo "Especificando permissões dos diretórios...."
 
-groupadd GRP_SEC 
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
 
- 
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
 
-echo "criando usuarios" 
-
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM 
-
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM 
-
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM 
-
- 
-
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN 
-
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN 
-
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN 
-
- 
-
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC 
-
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC 
-
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC 
-
- 
-
-echo "especificando permissoes dos diretorios" 
-
-chown root:GRP_ADM /adm 
-
-chown root:GRP_VEN /ven 
-
-chown root:GRP_SEC /sec 
-
- 
-
-chmod 770 /adm 
-
-chmod 770 /ven 
-
-chmod 770 /sec 
-
-chmod 770 /publico 
-
-echo "fim..." 
+echo "Fim....." 
